fb6f7823f66e14c4e4d6437c64fd008cf0f65d32
Sam Wong <theesamwong@gmail.com>
2021-10-19 14:03:15 -0700

    user auth

--- frontend/gjallarhorn.jsx
+++ frontend/gjallarhorn.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
+  const root = document.getElementById('main');
   ReactDOM.render(<h1>Hello World</h1>, root)
 })
